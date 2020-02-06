66e9553287d3c2b8535729dab72bbc72ff26dc09
Mitra <Mi7Ra@users.noreply.github.com>
2020-02-06 15:10:17 -0600

    testing

--- app_api/controllers/bagels.js
+++ app_api/controllers/bagels.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Bagel = mongoose.model('Bagel');
 
 const allBagels = async(req, res, next) => {
-    Bagel.findOne({name : "Regular"}).exec(function (err, bags){
+    Bagel.find({}).exec(function (err, bags){
         res
         .status(200)
         .json(bags)
