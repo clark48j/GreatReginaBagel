e31a5644868e85536dbf7ca056ce287b1e2c28ac
Mitra <Mi7Ra@users.noreply.github.com>
2020-02-06 00:46:40 -0600

    edit bagel controller

--- app_api/controllers/bagels.js
+++ app_api/controllers/bagels.js
@@ -9,28 +9,8 @@ const allBagels = async(req, res, next) => {
     })
 };
 
-const locationsReadOne = (req, res, next) => { 
-    Loc
-        .findById(req.params.locationid)
-        .exec((err, location) => {
-            if (!location) {
-                return res
-                    .status(404)
-                    .json({
-                      "message": "location not found"
-                    });
-                } else if (err) {
-                    return res
-                        .status(404)
-                        .json(err);
-                }
-                res
-                    .status(200)
-                    .json(location);
-        });   
-};
+
 
 module.exports = {
-    allBagels,
-    locationsReadOne,
+    allBagels
 };
